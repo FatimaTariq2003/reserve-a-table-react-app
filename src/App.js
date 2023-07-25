5b2bad39bbe2c4df663d8508219c8c2593ed3174
unknown <engr.hg@gmail.com>
2023-07-25 05:41:08 -0700

    Setting semantic tags

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-
 import './App.css';
+import Header from './header';
+import Main from './main';
+import Footer from './footer';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+  <>
+  <Header/>
+  <Main/>
+  <Footer/>
+  </>
   );
 }
 
